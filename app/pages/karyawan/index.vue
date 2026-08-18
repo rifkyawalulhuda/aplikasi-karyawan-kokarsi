f4bb59063ae454508bd725e041076fa331bbfdf5
Rifky Awalul Huda <rifkyawalulhuda@gmail.com>
2026-08-18 19:39:46 +0700

    Update index.vue

--- app/pages/karyawan/index.vue
+++ app/pages/karyawan/index.vue
@@ -426,11 +426,12 @@ const filteredData = computed(() => {
 })
 
 watch([statusFilter, locationFilter, departmentFilter, searchQuery], () => {
-  pagination.value.pageIndex = 0
+  table.value?.tableApi?.setPageIndex(0)
 })
 
-watch(() => pagination.value.pageSize, () => {
-  pagination.value.pageIndex = 0
+watch(() => pagination.value.pageSize, async () => {
+  await nextTick()
+  table.value?.tableApi?.setPageIndex(0)
 })
 </script>
 
@@ -549,10 +550,11 @@ watch(() => pagination.value.pageSize, () => {
           />
         </div>
         <UPagination
-          :default-page="(table?.tableApi?.getState().pagination.pageIndex || 0) + 1"
-          :items-per-page="table?.tableApi?.getState().pagination.pageSize"
+          :key="`pagination-${pagination.pageSize}`"
+          :page="pagination.pageIndex + 1"
+          :items-per-page="pagination.pageSize"
           :total="filteredData.length"
-          @update:page="(p: number) => table?.tableApi?.setPageIndex(p - 1)"
+          @update:page="(p: number) => { table?.tableApi?.setPageIndex(p - 1) }"
         />
       </div>
     </template>
